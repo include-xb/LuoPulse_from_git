f24da35a17086228d416601e3a1e46f78e91bc3c
include-xb <baijinyuan0603@outlook.com>
2025-01-23 16:19:43 +0800

    添加对zip文件谱面的读取, 谱面添加转场动画, 布置盒子

---
+++ 脚本/RunningData.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# 曲包字典: 歌名对应该歌曲所对应的压缩文件路径
+var pack_list: Dictionary = { }
+
+
+# config 文件记录内容
+# ui 音量
+var ui_volume: float = 0.0
+# 歌曲播放音量
+var audio_volume: float = 0.0
+# 背景亮度
+var bglight: float = 0.0
+# 音符流速
+var speed: float = 0.0
+# 用户名
+var user_name: String = "小小洛"
