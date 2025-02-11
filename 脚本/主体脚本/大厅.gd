977aa1543f4663df308646f5fee102f5866d4849
include-xb <baijinyuan0603@outlook.com>
2025-02-11 12:37:05 +0800

    所有按钮都失效了是什么鬼

--- 脚本/主体脚本/大厅.gd
+++ 脚本/主体脚本/大厅.gd
@@ -1,1 +1,22 @@
 extends Control
+
+
+
+# 信箱
+func _on_mail_pressed() -> void:
+	print("点击信箱")
+
+
+# 设置
+func _on_setting_pressed() -> void:
+	print("点击设置")
+
+
+# 演奏
+func _on_start_pressed() -> void:
+	print("点击演奏")
+
+
+# WORLD
+func _on_world_pressed() -> void:
+	print("点击WORLD")
